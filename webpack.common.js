ac42cc943c2cd9a04293be17695cb3a442b4e7bd
sub-prime <sub-pr1me9000@gmail.com>
2025-05-02 13:48:17 +0300

    feat: add html loader

--- webpack.common.js
+++ webpack.common.js
@@ -19,13 +19,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },      
+        type: "asset/resource"
+      },
     ],
   },
 };
